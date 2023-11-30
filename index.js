e83b1780d0d38c2cad3e178320b43c9deb3989b9
Labonya <1aklimakter@gmail.com>
2023-11-30 18:38:52 +0600

    I have checked the project finally

--- index.js
+++ index.js
@@ -180,7 +180,7 @@ async function run() {
         //get premium
         app.get("/allBioDataPremium", async (req, res) => {
 
-            const result = await editBioDataAllCollection.find({accountType:"premium"}).sort( { age: -1 }).toArray()
+            const result = await editBioDataAllCollection.find({accountType:"premium"}).sort( { age: 1 }).toArray()
             res.send(result)
         })
 
@@ -326,48 +326,6 @@ async function run() {
             }
         })
 
-        //using aggregate pipeline 
-        app.get("/statistic-result", async (req, res) => {
-            const result = await paymentCollection.aggregate([
-                {
-                    $unwind: "$requesterId"
-
-                },
-                {
-                    $lookup: {
-                        from: "allBioData",
-                        localField: "biodataType",
-                        foreignField: "neededId",
-                        as: "requestId"
-                    }
-                },
-                {
-                    $unwind: "$requestId"
-                },
-                {
-                    $group: {
-                        _id: "$requestId.biodataType",
-                        account: { $first: "$requestId.accountType" },
-                        quantity: { $sum: 1 },
-                        totalRevenue: { $sum: "$price" }
-                    }
-                },
-                {
-                    $project: {
-                        _id: 0,
-                        category: "$_id",
-                        quantity: "$quantity",
-                        account: "$account",
-                        totalRevenue: "$totalRevenue"
-                    }
-                }
-            ]).toArray()
-
-            res.send(result)
-        })
-
-
-
         //add to favorite
         app.post("/addToFavorite", async (req, res) => {
             try {
@@ -486,8 +444,8 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
 
